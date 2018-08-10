d99f2e9f6d37b09ebf607d4bb74f3e39f6419f8c
George Oliveira <ghao@cin.ufpe.br>
2018-08-10 00:06:03 -0300

    Fixing imports of MainModule

--- server/src/main.hmr.ts
+++ server/src/main.hmr.ts
@@ -1,11 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 
-import { AppModule } from './app.module';
+import { MainModule } from './main.module';
 
 declare const module: any;
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(MainModule);
 	app.enableCors();
   await app.listen(3000);
 
